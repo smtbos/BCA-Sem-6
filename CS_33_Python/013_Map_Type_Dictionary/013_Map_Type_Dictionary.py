3ec60a8e199fb63861e06bc9c94b1964f234a96b
smtbos <smtbos@gmail.com>
2021-02-04 14:31:49 +0530

    Python 013 Map Type Dictinary

---
+++ CS_33_Python/013_Map_Type_Dictionary/013_Map_Type_Dictionary.py
@@ -0,0 +1,14 @@
+d = {10: "hiren", 11: "nayan", 12: "smit"}
+print(d)
+print(d[10])
+print(d[12])
+del d[12]
+print(d)
+
+"""
+Output :
+{10: 'hiren', 11: 'nayan', 12: 'smit'}
+hiren
+smit
+{10: 'hiren', 11: 'nayan'}
+"""
