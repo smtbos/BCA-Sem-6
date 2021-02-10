7086f9da33b3ec2cb2e2837af518dcbc13b418a5
smtbos <smtbos@gmail.com>
2021-02-10 10:12:12 +0530

    Android 013 Brodcast Receiver

---
+++ CS_31_Android/013_Brodcast_Receiver/app/src/main/java/com/example/a013broadcastreceiver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a013broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ CS_31_Android/013_Brodcast_Receiver/app/src/main/java/com/example/a013broadcastreceiver/MyReceiver.java
@@ -0,0 +1,13 @@
+package com.example.a013broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Dear User Your Phone Battery Very Low!", Toast.LENGTH_SHORT).show();
+    }
+}
