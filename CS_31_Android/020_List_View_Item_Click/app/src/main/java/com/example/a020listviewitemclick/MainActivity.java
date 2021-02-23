63173a488fb3f7ca93209680d31521b9787eba67
smtbos <smtbos@gmail.com>
2021-02-23 15:11:59 +0530

    Android 020 List View Item Click

---
+++ CS_31_Android/020_List_View_Item_Click/app/src/main/java/com/example/a020listviewitemclick/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a020listviewitemclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView) findViewById(R.id.lv);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ""+lv.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
