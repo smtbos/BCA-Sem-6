f8ffd9f840b66b188ded2bcb70b3193453862bb8
smtbos <smtbos@gmail.com>
2021-02-24 19:41:13 +0530

    Python 030 Input Function

---
+++ CS_33_Python/030_Input_Function/030_Input_Function.py
@@ -0,0 +1,10 @@
+f = input("Enter Your Name    : ")
+l = input("Enter Your Surname : ")
+print("Your Name is :",f,l)
+
+"""
+Output:
+Enter Your Name    : smit
+Enter Your Surname : bosamiya
+Your Name is : smit bosamiya
+"""
