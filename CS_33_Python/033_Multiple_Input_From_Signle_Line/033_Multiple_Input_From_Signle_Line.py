d51320d0aea1c58a3fe603069a67ea99a4f457d9
smtbos <smtbos@gmail.com>
2021-02-24 20:32:58 +0530

    Python 033 Multiple Input From Single Line

---
+++ CS_33_Python/033_Multiple_Input_From_Signle_Line/033_Multiple_Input_From_Signle_Line.py
@@ -0,0 +1,13 @@
+l = [int(x) for x in input("Enter Numbers : ").split(' ')]
+print("Sum = ",sum(l))
+
+l = input("Enter String : ").split()
+print(','.join(l))
+
+"""
+Output:
+Enter Numbers : 10 30 50
+Sum =  90
+Enter String : KSC PAC AMRELI
+KSC,PAC,AMRELI
+"""
