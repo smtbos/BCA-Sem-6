d0874c36a0b68d67170c6935a4e77323cfa024ac
smtbos <smtbos@gmail.com>
2021-02-24 18:14:46 +0530

    Python 028 Math Library 2

---
+++ CS_33_Python/028_Math_Library_2/028_Math_Library_2.py
@@ -0,0 +1,17 @@
+from math import sqrt, factorial, ceil, floor, pow, pi
+print(sqrt(16))
+print(factorial(5))
+print(ceil(4.5))
+print(floor(4.5))
+print(pow(2,3))
+print("Area of Circle = ", pi*(15.5**2))
+
+"""
+Output:
+4.0
+120
+5
+4
+8.0
+Area of Circle =  754.7676350249478
+"""
