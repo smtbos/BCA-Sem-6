329de48fd6463755fed5ca5c2f667549ef05972e
smtbos <smtbos@gmail.com>
2021-02-24 20:21:10 +0530

    Python 032 Other Number to Decimal

---
+++ CS_33_Python/032_Other_Number_to_Decimal/032_Other_Number_to_Decimal.py
@@ -0,0 +1,13 @@
+print("Hex to Dec =",int(input("Enter Hex Number : "), 16))
+print("Oct to Dec =",int(input("Enter Oct Number : "), 8))
+print("Bin to Dec =",int(input("Enter Bin Number : "), 2))
+
+"""
+Output:
+Enter Hex Number : AB
+Hex to Dec = 171
+Enter Oct Number : 46
+Oct to Dec = 38
+Enter Bin Number : 10010
+Bin to Dec = 18
+"""
