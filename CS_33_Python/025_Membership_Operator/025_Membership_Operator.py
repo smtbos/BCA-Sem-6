79102895ab56e907ef1563d088320088d1886a2c
smtbos <smtbos@gmail.com>
2021-02-24 17:35:44 +0530

    Python 025 Membership Operator

---
+++ CS_33_Python/025_Membership_Operator/025_Membership_Operator.py
@@ -0,0 +1,14 @@
+# in Operator
+l = [10, 20, 30]
+print(20 in l)
+
+# not in Operator
+l = ['Smit', 'Urmil']
+if 'Jay' not in l:
+    print("Jay not in Group")
+
+"""
+Output:
+True
+Jay not in Group
+"""
