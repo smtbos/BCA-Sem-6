ac6dcae616aa20ed8bda9210abbfbd452a45c347
smtbos <smtbos@gmail.com>
2021-02-24 17:32:30 +0530

    Python 024 Bitwise Operator

---
+++ CS_33_Python/024_Bitwise_Operator/024_Bitwise_Operator.py
@@ -0,0 +1,34 @@
+print("\nBitwise Compliment ~")
+print("~10 = ", ~10)
+print("\nBitwise And &")
+print("10&11= ", 7&6)
+print("\nBitwise Or |")
+print("10|11 = ", 7|6)
+print("\nBitwise Xor ^")
+print("10^11 = ", 10^11)
+print("\nBitwise Shift Left <<")
+print("21<<2 = ", 21<<2)
+print("\nBitwise Shift Right >>")
+print("21>>2 = ", 21>>2)
+
+"""
+Output:
+
+Bitwise Compliment ~
+~10 =  -11
+
+Bitwise And &
+10&11=  6
+
+Bitwise Or |
+10|11 =  7
+
+Bitwise Xor ^
+10^11 =  1
+
+Bitwise Shift Left <<
+21<<2 =  84
+
+Bitwise Shift Right >>
+21>>2 =  5
+"""
