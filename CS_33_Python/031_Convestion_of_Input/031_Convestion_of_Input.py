58bb23975806a6ee56c6616fe4dd7a8e6a3a3ad6
smtbos <smtbos@gmail.com>
2021-02-24 19:55:02 +0530

    Python 031 Converstion of Input

---
+++ CS_33_Python/031_Convestion_of_Input/031_Convestion_of_Input.py
@@ -0,0 +1,17 @@
+a = int(input("Enter Value of A : "))
+b = int(input("Enter Value of B : "))
+
+print("Sum = ",a+b)
+print("Sub = ",a-b)
+print("Mul = ",a*b)
+print("Div = ",a/b)
+
+"""
+Output:
+Enter Value of A : 10
+Enter Value of B : 5
+Sum =  15
+Sub =  5
+Mul =  50
+Div =  2.0
+"""
