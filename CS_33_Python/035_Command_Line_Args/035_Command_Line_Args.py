16d49a73f1fa32b17caba987696a468fd0bbce92
smtbos <smtbos@gmail.com>
2021-02-25 17:29:20 +0530

    Python 035 Command Line Args

---
+++ CS_33_Python/035_Command_Line_Args/035_Command_Line_Args.py
@@ -0,0 +1,10 @@
+import sys
+print("Length =", len(sys.argv))
+print(sys.argv)
+
+"""
+Output:
+$ python 035_Command_Line_Args.py 10 smit "Ksc Psc"
+Length = 4
+['035_Command_Line_Args.py', '10', 'smit', 'Ksc Psc']
+"""
