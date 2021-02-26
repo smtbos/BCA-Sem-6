bd37ee931d6f37f06d288d5945520b542547c0e8
smtbos <smtbos@gmail.com>
2021-02-26 13:10:26 +0530

    Python 041 Identify Number Type

---
+++ CS_33_Python/041_Identity_Number_Type/041_Identity_Number_Type.py
@@ -0,0 +1,13 @@
+i = int(input("Enter Number : "))
+if i == 0:
+    print(i,"is Zero")
+elif i < 0:
+    print(i,"is Nagative")
+else:
+    print(i,"is Positive")
+
+"""
+Output:
+Enter Number : 5
+5 is Positive
+"""
