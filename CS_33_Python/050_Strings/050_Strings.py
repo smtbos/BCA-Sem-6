ccb0f2767f2c03d11345ae8d2794ad99b58b2531
smtbos <smtbos@gmail.com>
2021-03-15 23:28:31 +0530

    Python 050 Strings

---
+++ CS_33_Python/050_Strings/050_Strings.py
@@ -0,0 +1,28 @@
+str = "Core python"
+
+print(str)
+print(str[0:9:1])
+print(str[0:9:2])
+print(str[::2])
+print(str[2::])
+print(str[-4:-1])
+print(str[-6])
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
+
+"""
+Output:
+Core python
+Core pyth
+Cr yh
+Cr yhn
+re python
+tho
+p
+CORE PYTHON
+core python
+Core python
+Core Python
+"""
